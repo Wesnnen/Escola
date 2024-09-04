16410551ee61d5d6b57f9c86923f550fb4126e30
Wesnnen Silva <54326292+Wesnnen@users.noreply.github.com>
2024-09-04 19:11:05 -0300

    aula13
    aula 13

--- Escola/Pages/Diferenciais.cshtml.cs
+++ Escola/Pages/Diferenciais.cshtml.cs
@@ -5,8 +5,59 @@ namespace Escola.Pages
 {
     public class DiferenciaisModel : PageModel
     {
+        public List<Professor> x { get; set; } = new List<Professor>();
         public void OnGet()
         {
+            x = new List<Professor>
+    {
+        new Professor
+            {
+                Descricao = "",
+                ImagemUrl = "/images/Deadpool_&_Wolverine_cartaz.jpg"
+            },
+            new Professor
+            {
+                Descricao = "",
+                ImagemUrl = "/images/DivertidaMente2.jpeg"
+            },
+            new Professor
+            {
+                Descricao = "",
+                ImagemUrl = "/images/MeuMalvadoFavorito 4.jpg"
+            },
+            new Professor
+            {
+                Descricao = "",
+                ImagemUrl = "/images/OCorvo.webp"
+            },
+            new Professor
+            {
+                Descricao = "",
+                ImagemUrl = "/images/HellBoyeoHomemTorto.webp"
+            },
+            new Professor
+            {
+                Descricao = "",
+                ImagemUrl = "/images/LuccaseGiemDinossauros.jpg"
+            },
+            new Professor
+            {
+                Descricao = "",
+                ImagemUrl = "/images/LuccaseGiemDinossauros.jpg"
+            },
+            new Professor
+            {
+                Descricao = "",
+                ImagemUrl = "/images/LuccaseGiemDinossauros.jpg"
+            },
+            new Professor
+            {
+                Descricao = "",
+                ImagemUrl = "/images/LuccaseGiemDinossauros.jpg"
+            },
+
+        };
         }
     }
 }
+    
